298d47e09c127e0b8b2323e949b6bd1ac2a40dc6
Joseph Okafor <joptimalresource@live.com>
2022-08-10 15:26:02 +0100

    Added the favicon and titles for each pages.

--- components/layout/DashboardLayout.js
+++ components/layout/DashboardLayout.js
@@ -3,26 +3,34 @@ import LeftSection from "./dashboard-left-section";
 import RightSection from "./dashboard-right-section";
 import BodySection from "./dashboard-body-section";
 import styles from "./DashboardLayout.module.css";
+import Head from "next/head";
 
 const DashboardLayout = (props) => {
   return (
-    <div className={styles.container}>
-      <LeftSection></LeftSection>
-      <RightSection
-        title={props.title}
-        middleChildren={props?.middleChildren}
-        showMiddleButton={props?.showMiddleButton}
-        buttonVariant={props?.buttonVariant}
-        buttonLabel={props?.buttonLabel}
-        buttonUrl={props?.buttonUrl}
-        buttonSurface={props?.buttonSurface}
-        buttonClassNames={props?.buttonClassNames}
-        buttonLeftIcon={props?.buttonLeftIcon}
-        buttonRightIcon={props?.buttonRightIcon}
-      >
-        <BodySection>{props.children}</BodySection>
-      </RightSection>
-    </div>
+    <>
+      <Head>
+        <title>{`Dev Test - ${props?.title}`}</title>
+        <meta name="description" content="Dev Test Gig Dashboard" />
+        <link rel="icon" href="/images/svg/icon.svg" />
+      </Head>
+      <div className={styles.container}>
+        <LeftSection></LeftSection>
+        <RightSection
+          title={props?.title}
+          middleChildren={props?.middleChildren}
+          showMiddleButton={props?.showMiddleButton}
+          buttonVariant={props?.buttonVariant}
+          buttonLabel={props?.buttonLabel}
+          buttonUrl={props?.buttonUrl}
+          buttonSurface={props?.buttonSurface}
+          buttonClassNames={props?.buttonClassNames}
+          buttonLeftIcon={props?.buttonLeftIcon}
+          buttonRightIcon={props?.buttonRightIcon}
+        >
+          <BodySection>{props.children}</BodySection>
+        </RightSection>
+      </div>
+    </>
   );
 };
 
