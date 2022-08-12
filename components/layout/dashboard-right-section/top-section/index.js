e6b44947a78c047437dcd8ad1742e0b52e6bffb8
Joseph Okafor <joptimalresource@live.com>
2022-08-12 05:53:00 +0100

    Made it responsive

--- components/layout/dashboard-right-section/top-section/index.js
+++ components/layout/dashboard-right-section/top-section/index.js
@@ -13,32 +13,50 @@ const TopSection = (props) => {
     <div className={styles.container}>
       <div className={styles.fixedContainer}>
         <div className={styles.leftSection}>
+          <div className={styles.logoSection}>
+            <Image
+              src="/images/svg/full_logo.svg"
+              width="80px"
+              height="36px"
+              alt="Logo"
+              layout="fixed"
+            />
+          </div>
           {/* <input type="text" name="search" id="search" /> */}
-          <InputWithAdornment
-            leftIcon={
-              <Icon icon="akar-icons:search" className="text-[#8DA78C]" />
-            }
-            value={search}
-            onChange={(e) => setSearch(e?.target?.value)}
-            name="name"
-            className={styles.formcontrol}
-            placeholder="Search"
-            inputClassName={styles.inputWidth}
-          />
+          <div className={styles.InputWithAdornment}>
+            <InputWithAdornment
+              leftIcon={
+                <Icon icon="akar-icons:search" className="text-[#8DA78C]" />
+              }
+              value={search}
+              onChange={(e) => setSearch(e?.target?.value)}
+              name="name"
+              className={styles.formcontrol}
+              placeholder="Search"
+              inputClassName={styles.inputWidth}
+            />
+          </div>
         </div>
         <div className={styles.rightSection}>
           <section className={styles.tools}>
             <Icon icon="clarity:notification-outline-badged" />
-            <Icon icon="carbon:email" />
-            <Icon icon="ant-design:setting-outlined" />
+            <div className={styles.email}>
+              <Icon icon="carbon:email" />
+            </div>
+            <div className={styles.settings}>
+              <Icon icon="ant-design:setting-outlined" />
+            </div>
           </section>
-          <Image
-            src="/images/profile.jpg"
-            width="30px"
-            height="30px"
-            alt="Logo"
-            className={styles.profile}
-          />
+          <div className={styles.desktopProfile}>
+            <Image
+              src="/images/profile.jpg"
+              width="30px"
+              height="30px"
+              alt="Logo"
+              className={`${styles.profile}`}
+            />
+          </div>
+          <Icon icon="charm:menu-hamburger" className={styles.menu} />
         </div>
       </div>
     </div>
