9e8710d7784e8387a9301976704b690860fef141
Joseph Okafor <joptimalresource@live.com>
2022-08-22 19:02:49 +0100

    Testing the actions

--- components/user/account/index.js
+++ components/user/account/index.js
@@ -4,9 +4,7 @@ import React, { Fragment } from "react";
 const AccountComponent = () => {
   return (
     <Fragment>
-      <p>
-        This is the account section - testing out the github action workflow
-      </p>
+      <p>This is the account section - testing out the github action</p>
       <Link href="/user/gigs">
         <a>Click here to go to Gigs</a>
       </Link>
